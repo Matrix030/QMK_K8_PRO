622294cc30df90e6be30dd204e633213ca0ff0b7
Matrix030 <rishigharat@gmail.com>
2025-10-07 09:11:31 -0400

    new files

--- keyboards/keychron/k8_pro/ansi/rgb/config.h
+++ keyboards/keychron/k8_pro/ansi/rgb/config.h
@@ -15,6 +15,10 @@
  */
 
 #pragma once
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_MAX_SPEED 5        // Increase this (default is 10)
+#define MOUSEKEY_TIME_TO_MAX 20      // Decrease this for faster acceleration
 
 #ifdef RGB_MATRIX_ENABLE
 /* RGB Matrix driver configuration */
--- keyboards/keychron/k8_pro/rules.mk
+++ keyboards/keychron/k8_pro/rules.mk
@@ -3,7 +3,7 @@ OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 OPT_DEFS += -DNO_USB_STARTUP_CHECK -DENABLE_FACTORY_TEST
 
 SRC += matrix.c
-
+MOUSEKEY_ENABLE = yes
 include keyboards/keychron/bluetooth/bluetooth.mk
 
 
